59fa96fc2e44c66bd60b19e47c08de33fc50f6f8
nalssee <kenjin@sdf.org>
2017-12-17 22:08:01 +0900

    first init

---
+++ ch02.py
@@ -0,0 +1,19 @@
+#%%
+
+# Manually downloaded the file 'housing.csv'
+from sqlplus import *
+import matplotlib.pyplot as plt
+
+#%%
+
+
+with dbopen('db') as c:
+    c.write('housing.csv')
+
+#%%
+
+with dbopen('db') as c:
+    housing = c.rows('housing').df()
+    housing.hist(bins=50, figsize=(20, 15))
+    plt.show()
+#%%
